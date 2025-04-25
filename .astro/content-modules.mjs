fb1904a33c77555f8056fdf70f602ad61bc40d02
Munay-K <gonzalo.alfacaso@gmail.com>
2025-04-25 15:56:05 -0500

    new baseline

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,5 @@
+
+export default new Map([
+["src/content/docs/tutorials/elements.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ftutorials%2Felements.mdx&astroContentModuleFlag=true")],
+["src/content/proyectos/motor-3d.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproyectos%2Fmotor-3d.mdx&astroContentModuleFlag=true")]]);
+		
