2e477bc5447e3de2c70a882736707bac4c7a0031
Munay-K <gonzalo.alfacaso@gmail.com>
2025-04-25 12:15:35 -0500

    delete all

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  output: 'static', // For static sites (recommended)
-  site: 'https://your-site-name.netlify.app', // Your future Netlify URL
-});
